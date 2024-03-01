6a16991b5e97e122dfa96214487d12a261428eff
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2024-03-01 12:41:29 +0530

    Update nexus_startup_script.sh

--- bash_scripts/nexus_startup_script.sh
+++ bash_scripts/nexus_startup_script.sh
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Copyright (c) [2024], [DrillDevOps, Owner/Author: Kalyan Kumar Reddy Chitiki]
+# All rights reserved.
 
 # Exit script if any command fails
 set -e
